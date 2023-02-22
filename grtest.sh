b2960e09dc8b886c6862a1ae45fdc0e159268b27
SeyedMohsen Hosseini <Mshosse59@uwo.ca>
2023-02-22 14:27:31 -0500

    predict MSA and T5

--- grtest.sh
+++ grtest.sh
@@ -8,12 +8,6 @@
 #source /home/mohsenh/grDelphiTensorflow1GPUenv/bin/activate
 source /home/mohsenh/grCPuMsaEnv/bin/activate
 
-#python predictTF2ZeroPadMSA.py
-python predictTF2ZeroPadEnsSep.py
-#python predictTF2ZeroPadEnsumbleMSA.py
-#python predictTF2ZeroPadEnsumble.py
-#python predictTF2ZeroPadEnsumbleWholeProt.py
-#python predictTF2ZeroPadMultiTFlat.py
-#python predictTF2ZeroPadMultiRnnTf.py
-#python predictTF2MultiZeroPadResize.py
-#python predictPointNet.py
+#python predict_MSA_MLP.py
+#python predict_MSA_LSTM.py
+python predict_T5_LSTM.py
